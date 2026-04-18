0aae5ca3d66a2919b65601f43ac08a95b0089cda
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2026-04-18 20:39:21 +0300

    Move todo-related objects into todoObjects directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles.css';
-import { createTodoObject } from './createTodoObject.js';
-import { getTodoObjects } from './todoObjectsStorage.js';
+import { createTodoObject } from './todoObjects/createTodoObject.js';
+import { getTodoObjects } from './todoObjects/todoObjectsStorage.js';
 
 createTodoObject("Task 1", "Task 1 Description", "11/11/2026", "High");
 const todoObjects = getTodoObjects();
-alert(todoObjects[0].Title);
+alert(todoObjects[0].UUID);
---
+++ src/listObjects/createListObjects.js
@@ -0,0 +1,8 @@
+class List {
+
+    #name
+    
+    constructor() {
+        
+    }
+}
