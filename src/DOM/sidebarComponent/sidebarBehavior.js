270f1165c057fbbfc581678a166ea811fd326964
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2026-05-04 14:10:32 +0300

    Modify Sidebar Behavior
    Modified the sidebar behavior to the following to expand and collapse based on classes instead of disappearing
    Now 2 new classes are introduced: `sidebar-collapse` & `sidebar-expand`
    Each class hides and shows relevant elements from the same `aside` element

--- src/DOM/sidebarComponent/sidebarBehavior.js
+++ src/DOM/sidebarComponent/sidebarBehavior.js
@@ -11,22 +11,26 @@ import { getListObjects } from "../../objectsComponents/centralObjectsStorage.js
 const allListObjects = getListObjects();
 
 function collapseSidebar() {
-  aside.classList.add("display-none");
+  aside.classList.remove("aside-expanded");
+  aside.classList.add("aside-collapsed");
+  for (let i = 0; i < sidebarSections.length; i++) {
+    sidebarSections[i].classList.add("display-none");
+  }
   sidebarButtons[1].classList.remove("display-none");
   sidebarButtons[1].textContent = "→";
   sidebarButtons[1].classList.add("expand-button");
-  body.append(sidebarButtons[1]);
+  aside.append(sidebarButtons[1]);
   sidebarButtons[1].addEventListener("click", expandSidebar);
 }
 
 function expandSidebar() {
-  body.append(aside);
-  aside.classList.remove("display-none");
+  aside.classList.remove("aside-collapsed");
+  aside.classList.add("aside-expanded");
+  for (let i = 0; i < sidebarSections.length; i++) {
+    sidebarSections[i].classList.remove("display-none");
+  }
   sidebarButtons[1].classList.add("display-none");
   sidebarButtons[0].addEventListener("click", collapseSidebar);
 }
 
-export {
-  collapseSidebar,
-  expandSidebar,
-};
+export { collapseSidebar, expandSidebar };
